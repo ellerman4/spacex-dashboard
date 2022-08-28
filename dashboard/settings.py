c1f0c19f27416295469ff707411bc66883006d19
ellerman <106990217+ellerman4@users.noreply.github.com>
2022-08-28 13:16:27 -0500

    define static path

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/imgs'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
