e101b75c3c23722152026bdfc42387fdf70dc752
ellerman <106990217+ellerman4@users.noreply.github.com>
2022-08-31 18:14:20 -0500

    schedule func for getting starlink TLE

--- main/management/commands/update_data.py
+++ main/management/commands/update_data.py
@@ -1,21 +1,28 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-import time, os, schedule
+import time, os, schedule, requests
 
 
 class Command(BaseCommand):
     '''Deletes cache on an interval of 1 hour. This will force an API call 
     only when our fetch_data function will see no data in the cache folder.'''
 
-    help = 'Wipes cached json files'
+    help = 'Wipes cached json/txt files'
 
     def update_data():
-        print('deleting cache')
+        print('deleting cache...')
         dir = 'cache/'
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
 
+    def update_starlink():
+        data = requests.get('https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=tle').text
+        text_file = open("cache/starlink.txt", "w")
+        n = text_file.write(data)
+        text_file.close()
+
     schedule.every(1).hours.do(update_data)
+    schedule.every(1).hours.do(update_starlink)
 
     while True:
         schedule.run_pending()
