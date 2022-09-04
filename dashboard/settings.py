87733f2474c6e2161f20f5fc712092a9245d24ce
ellerman <106990217+ellerman4@users.noreply.github.com>
2022-09-03 20:32:25 -0500

    add csp middleware

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
 
@@ -209,3 +210,56 @@ STATICFILES_FINDERS += ('compressor.finders.CompressorFinder',)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSP settings
+
+# uri to report policy violations
+# uri to report policy violations
+CSP_REPORT_URI = '<add your reporting uri>'
+
+# default source as self
+CSP_DEFAULT_SRC = ("'self'", )
+
+# style from our domain and googleapis
+CSP_STYLE_SRC = ("'self'", 
+    "'unsafe-inline'",
+    "https://fonts.googleapis.com",)
+
+CSP_SCRIPT_SRC = ("'self'",
+        "'unsafe-eval'",
+        "'unsafe-inline'",
+        "https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js",
+        "cdn.tailwindcss.com",
+        "ajax.googleapis.com",
+        "cdnjs.cloudflare.com/ajax/libs/three.js/0.143.0/three.min.js",
+        "http://unpkg.com/satellite.js/dist/satellite.min.js",
+        "http://unpkg.com/globe.gl",
+        "https://unpkg.com/flowbite@1.4.0/dist/flowbite.js")
+
+CSP_FONT_SRC = ("'self'", "fonts.gstatic.com")
+
+CSP_IMG_SRC = ("'self'",
+        "imgur.com",
+        "i.imgur.com",
+        "images2.imgbox.com",
+        "*.staticflickr.com")
+
+# loading manifest, workers, frames, etc
+CSP_CONNECT_SRC = ("'self'", 
+    "www.google-analytics.com" )
+
+CSP_OBJECT_SRC = ("'self'", )   # specifies valid sources for the <object>, <embed>, and <applet> elements
+
+CSP_BASE_URI = ("'self'", )     # restricts the URLs which can be used in a document's <base> element
+
+CSP_FRAME_ANCESTORS = ("'self'", )  # specifies valid parents that may embed a page using <frame>, <iframe>, <object>, <embed>, or <applet>
+
+CSP_FORM_ACTION = ("'self'", )      # restricts the URLs which can be used as the target of form submissions from a given context
+
+CSP_INCLUDE_NONCE_IN = ('script-src', )
+
+CSP_MANIFEST_SRC = ("'self'", )     # specifies which manifest can be applied to the resource.
+
+CSP_WORKER_SRC = ("'self'", )       # specifies valid sources for Worker, SharedWorker, or ServiceWorker scripts.
+
+CSP_MEDIA_SRC = ("'self'", )        # specifies valid sources for loading media using the <audio> and <video> elements.
